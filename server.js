54f0d3e7774fc08876e187747241bf9c00092861
Oru-Bus <mat.lachambre@gmail.com>
2024-07-10 19:18:10 +0200

    update

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('startGame', (roomCode) => {
-        io.to(roomCode).emit('gameStarted');
+        io.to(roomCode).emit('gameStarted', rooms[roomCode].players);
     });
 });
 
