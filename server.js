c78824351f4f0a0e5697dbcc603a71adb51b29dc
Oru-Bus <mat.lachambre@gmail.com>
2024-07-11 18:23:07 +0200

    save

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let rooms = {};
 app.post('/create-room', (req, res) => {
     const roomCode = uuidv4().slice(0, 5).toUpperCase();
     const pseudo = req.body.pseudo;
-    rooms[roomCode] = { players: [] };
+    rooms[roomCode] = { players: [{ pseudo, inGame: false }], gameStarted: false };
     res.json({ roomCode });
 });
 
@@ -28,26 +28,43 @@ app.get('/room/:code', (req, res) => {
     }
 });
 
+app.get('/game/:code', (req, res) => {
+    const roomCode = req.params.code;
+    if (rooms[roomCode]) {
+        res.sendFile(__dirname + '/public/game.html');
+    } else {
+        res.status(404).send('Jeu non trouvé');
+    }
+});
+
 io.on('connection', (socket) => {
     let currentRoomCode = null;
     let currentPseudo = null;
+    let redirectedToGame = false;
 
     socket.on('joinRoom', (roomCode, pseudo) => {
         if (rooms[roomCode]) {
             currentRoomCode = roomCode;
             currentPseudo = pseudo;
+            redirectedToGame = false;
             socket.join(roomCode);
-            rooms[roomCode].players.push({ pseudo });
+
+            if (!rooms[roomCode].players.find(player => player.pseudo === pseudo)) {
+                rooms[roomCode].players.push({ pseudo, inGame: false });
+            }
+
             socket.emit('currentPlayers', rooms[roomCode].players.map(player => player.pseudo));
             socket.to(roomCode).emit('newPlayer', pseudo);
         }
     });
 
     socket.on('disconnect', () => {
-        if (currentRoomCode && rooms[currentRoomCode]) {
+        if (currentRoomCode && rooms[currentRoomCode] && !redirectedToGame) {
             rooms[currentRoomCode].players = rooms[currentRoomCode].players.filter(player => player.pseudo !== currentPseudo);
-            if (rooms[currentRoomCode].players.length === 0) {
+            console.log(`Player ${currentPseudo} left room ${currentRoomCode}`, rooms[currentRoomCode].players);
+            if (rooms[currentRoomCode].players.length === 0 && !rooms[currentRoomCode].gameStarted) {
                 delete rooms[currentRoomCode];
+                console.log(`Room ${currentRoomCode} deleted`);
             } else {
                 socket.to(currentRoomCode).emit('playerLeft', currentPseudo);
             }
@@ -55,7 +72,21 @@ io.on('connection', (socket) => {
     });
 
     socket.on('startGame', (roomCode) => {
-        io.to(roomCode).emit('gameStarted', rooms[roomCode].players);
+        if (rooms[roomCode]) {
+            redirectedToGame = true;
+            rooms[roomCode].players.forEach(player => player.inGame = true);
+            rooms[roomCode].gameStarted = true;
+            console.log(rooms[roomCode].players);
+            io.to(roomCode).emit('gameRedirect', roomCode);
+        }
+    });
+
+    socket.on('gameStarted', (roomCode) => {
+        if (rooms[roomCode]) {
+            console.log(rooms[roomCode].players);
+            const playerPseudos = rooms[roomCode].players;
+            io.to(roomCode).emit('playerPseudos', playerPseudos);
+        }
     });
 });
 
